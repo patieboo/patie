684e30cb1029f36cf85798a18859a98bf9bce43b
paticia <patricia.nambozo1@gmail.com>
2021-05-06 06:46:27 -0700

    comment

---
+++ javascript.js
@@ -0,0 +1,26 @@
+//COVID CHECK
+var temp=33;
+if (temp==33) {
+    console.log("Normal temperature");
+}  
+else if (temp>=37) {
+console.log("Please test for covid-19");
+}
+else if (temp==35) {
+console.log("Please watch your temperature!");
+}
+else {
+console.log("Low temperature");
+}
+var i;
+i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+
+i[9] = "Armstrong";
+console.log(i[3]);
+
+i.reverse();
+
+console.log(i);
+
+
+
