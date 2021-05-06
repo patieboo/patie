d536db73e7f6bb7d10abaa4946ef7f5e5254ba49
Patricia Nambozo <82018123+patieboo@users.noreply.github.com>
2021-05-06 07:16:50 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -12,14 +12,12 @@ console.log("Please watch your temperature!");
 else {
 console.log("Low temperature");
 }
+
 var i;
 i = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
-
 i[9] = "Armstrong";
 console.log(i[3]);
-
 i.reverse();
-
 console.log(i);
 
 
